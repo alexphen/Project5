b9e036642aa54937ece2a1cbb5a0bc729ad8f621
alexphen <alexphen@vt.edu>
2017-04-18 17:33:51 -0400

    Add files via upload

--- GlyphList.java
+++ GlyphList.java
@@ -3,7 +3,6 @@
  */
 package musicpreference;
 
-
 /**
  * @author aphen
  * @version <4/12/17>
@@ -11,10 +10,20 @@ package musicpreference;
 public class GlyphList extends DLList<Glyph> {
     public static Song defaultSong = new Song("All These Things I've Done",
         "The Killers", 2005, "Alternative");
+    public static Song defaultSong1 = new Song("Upside Down", "Diana Ross",
+        1980, "disco");
+    public static Song defaultSong2 = new Song("Uma Thurman", "Fall Out Boy",
+        2015, "rock");
 
 
     public GlyphList() {
         super();
+    }
+    
+    public void setSorts(String str) {
+        for (int i = 0; i < size(); i++) {
+            this.get(i).setSort(str);
+        }
     }
 
 
@@ -25,19 +34,20 @@ public class GlyphList extends DLList<Glyph> {
 
     public static GlyphList defaultGlyphList() {
         GlyphList dGL = new GlyphList();
-        dGL.add(new Glyph(100, 100, 100, defaultSong));
+        dGL.add(new Glyph(defaultSong, 100, 100));
+        dGL.add(new Glyph(defaultSong1, 100, 100));
+        dGL.add(new Glyph(defaultSong2, 100, 100));
         return dGL;
     }
 
-    
 
     public void sortArtist() {
         for (int i = 1; i < this.size(); i++) {
             for (int k = i; k > 0; k--) {
                 Glyph curr = this.get(k);
                 Glyph before = this.get(k - 1);
-                if (curr.getSong().getArtist().compareTo(before.getSong()
-                    .getArtist()) < 0) {
+                if (curr.getSong().getArtistName().compareTo(before.getSong()
+                    .getArtistName()) < 0) {
                     swapWithBefore(k);
                 }
             }
@@ -64,8 +74,8 @@ public class GlyphList extends DLList<Glyph> {
             for (int k = i; k > 0; k--) {
                 Glyph curr = this.get(k);
                 Glyph before = this.get(k - 1);
-                if (curr.getSong().getTitle().compareTo(before.getSong()
-                    .getTitle()) < 0) {
+                if (curr.getSong().getSongTitle().compareTo(before.getSong()
+                    .getSongTitle()) < 0) {
                     swapWithBefore(k);
                 }
             }
@@ -78,7 +88,8 @@ public class GlyphList extends DLList<Glyph> {
             for (int k = i; k > 0; k--) {
                 Glyph curr = this.get(k);
                 Glyph before = this.get(k - 1);
-                if (curr.getSong().getYear() > before.getSong().getYear()) {
+                if (curr.getSong().getYearReleased() > before.getSong()
+                    .getYearReleased()) {
                     swapWithBefore(k);
                 }
             }
