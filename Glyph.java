5550044e654dfbec626036af82be5735023b74d4
Adam <adamt@vt.edu>
2017-04-18 16:10:34 -0400

    Add files via upload

--- Glyph.java
+++ Glyph.java
@@ -1,6 +1,7 @@
 package itemclasses;
 
 import java.awt.Color;
+import java.util.NoSuchElementException;
 import CS2114.Shape;
 import CS2114.TextShape;
 
@@ -10,131 +11,192 @@ import CS2114.TextShape;
  * @version 04/18/17
  */
 
-public class Glyph extends DLList<Shape>{
+public class Glyph extends DLList<Shape> {
 
-    
+    /**
+     * Border of the Glyph
+     */
     private Shape border;
+
+    /**
+     * Title for the glyph
+     */
     private TextShape title;
-    private DLList<Shape> glyph;
-    
+
+    /**
+     * List of shapes for this glyph
+     */
+    private DLList<Shape> glyphShapes;
+
     /**
      * The song associated with this glyph
      */
     private Song song;
-    
+
     /**
      * The array of listen and like values associated with each enum
      * depending on which boolean (hobby, major, state) is true
      */
     private int[] listenLike;
-    
+
     /**
      * The boolean for when the window is sorting by hobby
      */
     private boolean hobby;
-    
+
     /**
      * The boolean for when the window is sorting by major
      */
     private boolean major;
-    
+
     /**
      * The boolean for when the window is sorting by state
      */
     private boolean state;
-    
+
     /**
      * The listen side of the top bar
      */
     private Bar listen1;
-    
+
     /**
      * The like side of the top bar
      */
     private Bar like1;
-    
+
     /**
      * The listen side of bar 2
      */
     private Bar listen2;
-    
+
     /**
      * The like side of bar 2
      */
     private Bar like2;
-    
+
     /**
      * the listen side of bar 3
      */
     private Bar listen3;
-    
+
     /**
      * The like side of bar 3
      */
     private Bar like3;
-    
+
     /**
      * The listen side of bar 4
      */
     private Bar listen4;
-    
+
     /**
      * The like side of bar 4
      */
     private Bar like4;
-    
+
     private final int WIDTH = 100;
     private final int HEIGHT = 100;
-    
-    
+
+
     /**
      * The constructor for the glyph class
      */
     public Glyph(Song song, int x, int y) {
+        glyphShapes = new DLList<>();
         this.song = song;
         listenLike = new int[6];
         border = new Shape(x, y, WIDTH, HEIGHT, Color.WHITE);
-        title = new TextShape(x + 5, y + 5, song.getSongTitle() + "\nby the" + song.getArtistName());   
+        title = new TextShape(x + 5, y + 5, song.getSongTitle() + "\nby the"
+            + song.getArtistName());
         title.setBackgroundColor(Color.WHITE);
+        glyphShapes.add(border);
+        glyphShapes.add(title);
     }
-    
+
+
     /**
      * Returns the song associated with this glyph object
+     * 
      * @return Song the song associated with this glyph
      */
     public Song getSong() {
         return song;
     }
-    
+
+
     /**
      * returns the song associated with this glyph
      */
     public String getSongString() {
         return song.toString();
     }
+
+
+    /**
+     * Get hobby, for testing purposes
+     * 
+     * @return boolean the value of hobby
+     */
+    public boolean getHobby() {
+        return hobby;
+    }
+
+
+    /**
+     * Get major, for testing purposes
+     * 
+     * @return boolean the value of major
+     */
+    public boolean getMajor() {
+        return major;
+    }
+
+
+    /**
+     * Get state, for testing purposes
+     * 
+     * @return boolean the value of state
+     */
+    public boolean getState() {
+        return state;
+    }
     
+    /**
+     * Gets the listenLike array, for testing purposes
+     * 
+     * @return int[] the ListenLike array
+     */
+    public int[] getListenLike() {
+        return listenLike;
+    }
+
+
     /**
      * Updates the array of listenLike
      * Updates the booleans
      * Updates the bars
-     * @param num represents what bars need to be shown
-     * 1 for hobby
-     * 2 for major
-     * 3 for state
+     * 
+     * @param num
+     *            represents what bars need to be shown
+     *            1 for hobby
+     *            2 for major
+     *            3 for state
      */
     public void update(int num) {
         updateBools(num);
-        updateListenLike();  
+        updateListenLike();
         updateBars();
     }
-    
-    
+
+
     /**
      * Changes the boolean variables
-     * @param num determines what boolean will become true
+     * 
+     * @param num
+     *            determines what boolean will become true
      */
     private void updateBools(int num) {
-        
+
         switch (num) {
             case 1:
                 hobby = true;
@@ -158,7 +220,8 @@ public class Glyph extends DLList<Shape>{
                 break;
         }
     }
-    
+
+
     /**
      * Changes the values in listen and like
      */
@@ -169,33 +232,41 @@ public class Glyph extends DLList<Shape>{
         else if (major) {
             listenLike = song.getMajor();
         }
-        else {
+        else if (state) {
             listenLike = song.getState();
         }
+        else {
+            listenLike = null;
+        }
     }
-    
+
+
     /**
      * Changes the size of the bars
-     * possibly need to change the bars in 
+     * possibly need to change the bars in
      */
     private void updateBars() {
-        listen1 = new Bar(listenLike[0]);
-        like1 = new Bar((listenLike[1]/listenLike[0]) * 100);
-        listen2 = new Bar(listenLike[2]);
-        like2 = new Bar((listenLike[3]/listenLike[2]) * 100);
-        listen3 = new Bar(listenLike[4]);
-        like3 = new Bar((listenLike[5]/listenLike[4]) * 100);
-        listen4 = new Bar(listenLike[6]);
-        like4 = new Bar((listenLike[7]/listenLike[6]) * 100);    
-        
-        
-        glyph.add(listen1);
-        glyph.add(like1);
-        glyph.add(listen2);
-        glyph.add(like2);
-        glyph.add(listen3);
-        glyph.add(like3);
-        glyph.add(listen4);
-        glyph.add(like4);
+        if (listenLike != null) {
+            listen1 = new Bar(listenLike[0]);
+            like1 = new Bar((listenLike[1] / listenLike[0]) * 100);
+            listen2 = new Bar(listenLike[2]);
+            like2 = new Bar((listenLike[3] / listenLike[2]) * 100);
+            listen3 = new Bar(listenLike[4]);
+            like3 = new Bar((listenLike[5] / listenLike[4]) * 100);
+            listen4 = new Bar(listenLike[6]);
+            like4 = new Bar((listenLike[7] / listenLike[6]) * 100);
+
+            glyphShapes.add(listen1);
+            glyphShapes.add(like1);
+            glyphShapes.add(listen2);
+            glyphShapes.add(like2);
+            glyphShapes.add(listen3);
+            glyphShapes.add(like3);
+            glyphShapes.add(listen4);
+            glyphShapes.add(like4);
+        }
+        else {
+            throw new NoSuchElementException();
+        }
     }
 }
--- GlyphTest.java
+++ GlyphTest.java
@@ -1,5 +1,167 @@
 package itemclasses;
 
-public class GlyphTest {
+import java.util.NoSuchElementException;
+
+/**
+ * 
+ * @author Adam Tapp adamt
+ * @version 04/18/2017
+ */
+public class GlyphTest extends student.TestCase {
+    /**
+     * Glyph for testing
+     */
+    private Glyph glyph;
+
+    /**
+     * Song for Testing
+     */
+    private Song song;
+
+
+    /**
+     * Sets up the initial conditions for testing
+     */
+    public void setUp() {
+        song = new Song();
+        glyph = new Glyph(song, 1, 1);
+    }
+
+
+    /**
+     * Tests get song, which gets the song associated with this glyph
+     */
+    public void testGetSong() {
+        Song song1 = glyph.getSong();
+        assertEquals("Don't Stop Believing,Billy Joel,1925,HipHop", song1
+            .toString());
+        assertEquals("Don't Stop Believing,Billy Joel,1925,HipHop", glyph
+            .getSongString());
+    }
+
+
+    /**
+     * Tests update, which changes the booleans, array of listen and like values
+     * and updates the bar sizes
+     */
+    public void testUpdateBools() {
+        // Adds fake values to the arrays of the song
+        song.getHobbies()[0] = 100;
+        song.getHobbies()[1] = 83;
+        song.getHobbies()[2] = 38;
+        song.getHobbies()[3] = 23;
+        song.getHobbies()[4] = 92;
+        song.getHobbies()[5] = 23;
+        song.getHobbies()[6] = 45;
+        song.getHobbies()[7] = 34;
+
+        song.getMajor()[0] = 55;
+        song.getMajor()[1] = 21;
+        song.getMajor()[2] = 70;
+        song.getMajor()[3] = 52;
+        song.getMajor()[4] = 91;
+        song.getMajor()[5] = 32;
+        song.getMajor()[6] = 70;
+        song.getMajor()[7] = 21;
+
+        song.getState()[0] = 68;
+        song.getState()[1] = 12;
+        song.getState()[2] = 32;
+        song.getState()[3] = 12;
+        song.getState()[4] = 65;
+        song.getState()[5] = 7;
+        song.getState()[6] = 97;
+        song.getState()[7] = 54;
+
+        // Case 1: When the number isn't 1-3
+        try {
+            glyph.update(4);
+        }
+        catch (NoSuchElementException e) {
+            assertEquals(NoSuchElementException.class, e.getClass());
+        }
+        assertFalse(glyph.getHobby());
+        assertFalse(glyph.getMajor());
+        assertFalse(glyph.getState());
+
+        glyph.update(1);
+        assertTrue(glyph.getHobby());
+        assertFalse(glyph.getMajor());
+        assertFalse(glyph.getState());
+
+        glyph.update(2);
+        assertFalse(glyph.getHobby());
+        assertTrue(glyph.getMajor());
+        assertFalse(glyph.getState());
+
+        glyph.update(3);
+        assertFalse(glyph.getHobby());
+        assertFalse(glyph.getMajor());
+        assertTrue(glyph.getState());
+    }
+
+
+    /**
+     * Tests update, which changes the booleans, array of listen and like values
+     * and updates the bar sizes
+     */
+    public void testUpdateListenLike() {
+        // Adds fake values to the arrays of the song
+        song.getHobbies()[0] = 100;
+        song.getHobbies()[1] = 83;
+        song.getHobbies()[2] = 38;
+        song.getHobbies()[3] = 23;
+        song.getHobbies()[4] = 92;
+        song.getHobbies()[5] = 23;
+        song.getHobbies()[6] = 45;
+        song.getHobbies()[7] = 34;
+
+        song.getMajor()[0] = 55;
+        song.getMajor()[1] = 21;
+        song.getMajor()[2] = 70;
+        song.getMajor()[3] = 52;
+        song.getMajor()[4] = 91;
+        song.getMajor()[5] = 32;
+        song.getMajor()[6] = 70;
+        song.getMajor()[7] = 21;
+
+        song.getState()[0] = 68;
+        song.getState()[1] = 12;
+        song.getState()[2] = 32;
+        song.getState()[3] = 12;
+        song.getState()[4] = 65;
+        song.getState()[5] = 7;
+        song.getState()[6] = 97;
+        song.getState()[7] = 54;
+        
+        int[] hobby = song.getHobbies();
+        int[] major = song.getMajor();
+        int[] state = song.getState();
+
+        // Ensures the correct array is accessed based on which boolean is true
+        glyph.update(1);        
+        assertTrue(glyph.getListenLike().equals(hobby));
+        
+        glyph.update(2);
+        assertTrue(glyph.getListenLike().equals(major));
+        
+        glyph.update(3);
+        assertTrue(glyph.getListenLike().equals(state));
+        
+        try {
+            glyph.update(4);
+        }
+        catch (NoSuchElementException e) {
+            assertEquals(NoSuchElementException.class, e.getClass());
+        }
+        assertNull(glyph.getListenLike());
+        
+        
+    }
+
+    /**
+     * Tests update, which changes the booleans, array of listen and like values
+     * and updates the bar sizes
+     */
 
 }
